e954166f65c4875f2338482cb4ffa72f28149a62
HarenaFiantso <fiantsorav@gmail.com>
2023-09-28 17:16:50 +0300

    style: Styling the main style of the Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.scss";
 
 export default function Navbar() {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span className="text">Talenta</span>
+          <span className="domain">.mg</span>
+        </div>
+        <div className="links">
+          <span>Talenta business</span>
+          <span>Explore</span>
+          <span>Sign in</span>
+          <span>Become a seller</span>
+          <button>Join</button>
         </div>
       </div>
+      {
+        <>
+          <hr />
+          <div className="menu">
+            <span>Graphic design</span>
+            <span>Videography</span>
+            <span>Developer</span>
+            <span>Photography</span>
+          </div>
+        </>
+      }
     </div>
   );
 }
