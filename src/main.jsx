4289d51553e39f58dbc1282f995022cf50d67816
HarenaFiantso <fiantsorav@gmail.com>
2023-09-28 16:12:16 +0300

    style: Change the main method of styling by using sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App.jsx";
-import theme from "./theme/Theme.js";
-
-import { ThemeProvider } from "@mui/material/styles";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
+  <React.StrictMode>
     <App />
-  </ThemeProvider>
+  </React.StrictMode>
 );
