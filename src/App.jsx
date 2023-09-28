c56cfa08ea1adc1a3ddb8450f45648eaafef9266
HarenaFiantso <fiantsorav@gmail.com>
2023-09-28 19:44:17 +0300

    chore: Implement the application with react router dom and adding footer as a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/homePage/Home";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet/>
+        <Footer />
+      </>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router}/>
     </div>
   );
 }
