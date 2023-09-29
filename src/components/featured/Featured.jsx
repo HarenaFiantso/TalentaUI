5713b245664eee59e6ec23f16a97f15860933210
HarenaFiantso <fiantsorav@gmail.com>
2023-09-29 14:30:02 +0300

    chore: Finish the implementation of three JS

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -62,12 +62,18 @@ export default function Featured() {
           </div>
         </div>
         <div className="right">
-          <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+          <Canvas>
             <OrbitControls enableZoom={false} />
-            <ambientLight intensity={1} />
-            <directionalLight position={[3, 2, 1]} />
-            <Sphere args={[1, 100, 200]} scale={1} />
-            <MeshDistortMaterial />
+            <ambientLight intensity={2} />
+            <directionalLight position={[1, 6, 10]} />
+            <Sphere args={[1, 100, 200]} scale={1.8}>
+              <MeshDistortMaterial
+                color="#c83560"
+                attach="material"
+                distort={0.5}
+                speed={2}
+              />
+            </Sphere>
           </Canvas>
           <img src={bannerPic} alt="banner" />
         </div>
