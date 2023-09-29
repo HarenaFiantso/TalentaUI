3e3a6c249d4f24b34ac71b66dc7c0a890175aad3
HarenaFiantso <fiantsorav@gmail.com>
2023-09-29 09:29:05 +0300

    Day 2

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,19 +3,30 @@ import "./Featured.scss";
 
 import bannerPic from "../../assets/banner.png";
 import searchIcon from "../../assets/search.png";
+import { Button } from "@mui/material";
 
 export default function Featured() {
   return (
     <div className="featured">
       <div className="container">
         <div className="left">
-          <span className="text">Find the perfect <span>freelance services</span> for your business.</span>
+          <span className="text">
+            Find the perfect <span> <br /> freelance services</span> <br /> for your business.
+          </span>
           <div className="search">
             <div className="searchInput">
               <img src={searchIcon} alt="search Icon" />
               <input type="text" placeholder="Try building mobile app" />
             </div>
-            <button>Search</button>
+            <Button
+              sx={{
+                textTransform: "capitalize",
+                borderRadius: "0 5px 5px 0",
+                fontFamily: "Poppins",
+              }}
+            >
+              Search
+            </Button>
           </div>
           <div className="popular">
             <span>Popular:</span>
---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Slide.scss";
+
+export default function Slide() {
+  return (
+    <div className="slide">
+      <div className="container"></div>
+    </div>
+  );
+}
