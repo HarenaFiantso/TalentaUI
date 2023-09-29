4ead275164f3085e170a09e5d7e08ebb2e1cef04
HarenaFiantso <fiantsorav@gmail.com>
2023-09-29 14:58:45 +0300

    feat: Create a javascript file to store static data

---
+++ src/StaticData.js
@@ -0,0 +1,59 @@
+import imageOne from "./assets/cardsDummyData/service1.png";
+import imageTwo from "./assets/cardsDummyData/service2.png";
+import imageThree from "./assets/cardsDummyData/service3.png";
+import imageFour from "./assets/cardsDummyData/service4.png";
+import imageFive from "./assets/cardsDummyData/service5.png";
+import imageSix from "./assets/cardsDummyData/service6.png";
+import imageSeven from "./assets/cardsDummyData/service7.png";
+import imageEight from "./assets/cardsDummyData/service8.png";
+
+export const cards = [
+  {
+    id: 1,
+    title: "AI Artist",
+    description: "Add talent to AI",
+    img: { imageOne },
+  },
+  {
+    id: 2,
+    title: "Logo Design",
+    description: "Build your brand",
+    img: { imageTwo },
+  },
+  {
+    id: 3,
+    title: "WordPress",
+    description: "Customize your site",
+    img: { imageThree },
+  },
+  {
+    id: 4,
+    title: "Voice cover",
+    description: "Share you voice",
+    img: { imageFour },
+  },
+  {
+    id: 5,
+    title: "Book covers",
+    description: "Customize your book cover",
+    img: { imageFive },
+  },
+  {
+    id: 6,
+    title: "Translation",
+    description: "Go global",
+    img: { imageSix },
+  },
+  {
+    id: 7,
+    title: "Data Entry",
+    description: "Learn your business",
+    img: { imageSeven },
+  },
+  {
+    id: 8,
+    title: "Book Covers",
+    description: "Show case your story",
+    img: { imageOne },
+  },
+];
